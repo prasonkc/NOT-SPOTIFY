a9acb4f78782eacd88c68ca01bd81c0872b0fdef
Prason <prasonkc07.com>
2024-02-27 00:46:35 -0500

    made left playlist work

--- script.js
+++ script.js
@@ -16,16 +16,54 @@ async function getSongs(){
     return songs;
 }
 
+async function addSongsToLibrary(songs){
+    let songLibrary = document.querySelector(".songlibrary")
+    for (const i of songs) {
+        let song = i.split("/songs/")[1].replaceAll("%20", " ").replaceAll("_", " ")
+        songLibrary.innerHTML +=  `<li>
+                                    <div class="info flex items-center justify-center gap10">
+                                        <img src="./icons/music-icon.svg" alt="" srcset="" class="invert">
+                                        <p>${song}</p>
+                                    </div>
+                                    <div class="lib-play-btn flex items-center justify-center">
+                                        <img src="./icons/play.svg" alt="" srcset="" class="invert">
+                                    </div>
+                                    </li>`;
+    }
+}
+
+function listenForLibClicks(songs){
+    let libPlayBtns = document.querySelectorAll(".lib-play-btn");
+    let currentlyPlaying;
+
+    for (let i = 0; i < libPlayBtns.length; i++) {
+        let clickedAudio = new Audio(songs[i]);
+        let button = libPlayBtns[i];
+        button.addEventListener("click", (e) => {
+            if(currentlyPlaying && currentlyPlaying != clickedAudio){
+                currentlyPlaying.pause();
+            }
+            clickedAudio.play()
+            currentlyPlaying = clickedAudio;
+        })
+    }
+}
+
+
 async function main(){
     let songs = await getSongs();
+    await addSongsToLibrary(songs);
 
-    //dummy test lol
-    let audio = new Audio(songs[0]);
-    document.getElementById("play-button").addEventListener("click", (e) =>{
-        audio.play();
-    })
+    //listen for click on left library
+    listenForLibClicks(songs);
 
-    console.log(songs)
+    //listen for playlist clicks
+
+    //listen for controls
+    
+
+    // console.log(songs)
+
 }
 
 main()
